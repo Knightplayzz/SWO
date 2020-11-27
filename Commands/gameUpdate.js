8bf4cd543ed33bc7697e6510aa3011469bab8c23
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2020-11-27 20:17:05 +0100

    Update gameUpdate.js

--- Commands/gameUpdate.js
+++ Commands/gameUpdate.js
@@ -3,11 +3,11 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     var helloEmbed = new discord.MessageEmbed()
-    .setTitle("**Update info**")
+    .setTitle("**Game update info**")
     .setColor("GREEN")
     .setTimestamp()
     .setFooter(`© created by philippe#0354`)
-    .setDescription(`**What is new in this update?** \n !ticket, !new, !close, !announce fix, !role fix, !tempmute fix, role fixes, !update, !howcommand, !invite, welcome embed \n \n**When is the next update?** \n The next update is **23-11-2020**. \n \n **What is new in that update?** \n **Reaction roles**.`);
+    .setDescription(`**What is new?** World 1 base \n\n **When is the next update?** \n The next update is **1-12-2020**. \n \n **What is new in that update?** \n Hole world 1 done.`);
 
     message.channel.send(helloEmbed)
 
